394d2156ad6b3b9ba1742ee8007452261fbb1c25
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2021-07-26 22:11:30 +0900

    fixed code

--- src/board.ts
+++ src/board.ts
@@ -11,22 +11,19 @@ const board = [
 ]
 export const createBoard = () => {
   const root = document.getElementById("root")
-for (let i = 0; i < 9; i++) {
-  board[i] = new Array()
-  
-  const boards = document.createElement("div")
-  boards.id = "board"
-  boards.classList.add("board")
-  
-  for (let j = 0; j < 9; j++) {
-    board[i][j] = i * 9 + (j + 1)
-    const mathu = document.createElement("div")
-    mathu.id = "mathu"
-    mathu.classList.add("mathu")
-    boards.appendChild(mathu)
-
-  }
-  root!.appendChild(boards)
+    for (let i = 0; i < 9; i++) {
+      board[i] = new Array()
+      const boards = document.createElement("div")
+      boards.id = "board"
+      boards.classList.add("board")
+      root!.appendChild(boards)
+      for (let j = 0; j < 9; j++) {
+        board[i][j] = i * 9 + (j + 1)
+        const mathu = document.createElement("div")
+        mathu.id = "mathu"
+        mathu.classList.add("mathu")
+        boards.appendChild(mathu)
+      }
 }
 
 }
